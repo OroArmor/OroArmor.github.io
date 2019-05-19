217370eab0ddb9deb1b5f71c31ca5d1bd8996e93
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2019-05-18 20:03:29 -0700

    trying to clean up code. hopefully this works

--- javascript/BlogClass.js
+++ javascript/BlogClass.js
@@ -1,27 +1,3 @@
-let blogs;
-let blogObjs;
-
-fetch("./blog/blogIndex.json")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {blogs = data.blogs;})
-  .then(() => {
-	blogObjs=[];
-	for(blog in blogs){
-		blogObjs.push(new Blog(blogs[blog]));
-	}
-
-	for(var i = blogObjs.length-3; i < blogObjs.length; i++){
-		blogObjs[i].addBlogTo("#blogs", {class:"blog", id:"blogID"+(blogObjs.length-i)});
-	}
-  })
-  .catch(
-		  err => {
-			  alert(err);
-			  }
-		);
-
 class Blog {
   constructor(jsonData) {
     this.id = jsonData.id;
@@ -54,3 +30,5 @@ class Blog {
     });
   }
 }
+
+export {Blog};
---
+++ javascript/home.js
@@ -0,0 +1,23 @@
+import {Blog} from "./BlogClass.js";
+alert("no worky");
+
+let blogs;
+let blogObjs;
+
+fetch("./blog/blogIndex.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {blogs = data.blogs;})
+  .then(() => {
+	blogObjs=[];
+	for(blog in blogs){
+		blogObjs.push(new Blog(blogs[blog]));
+	}
+
+	for(var i = blogObjs.length-3; i < blogObjs.length; i++){
+		blogObjs[i].addBlogTo("#blogs", {class:"blog", id:"blogID"+(blogObjs.length-i)});
+	}
+	console.log("Finished");
+  })
+  .catch(err => {alert(err);});
