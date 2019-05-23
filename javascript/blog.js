bf99596811e456efe57f4908d6d68616bc97af67
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2019-05-23 14:01:27 -0700

    fixing blogs

--- javascript/blog.js
+++ javascript/blog.js
@@ -2,7 +2,7 @@ let blogs;
 let blogObjs;
 
 whenLoaded = () => {
-  fetch("./blogIndex.json")
+  fetch("https://oroarmor.github.io/blog/blogIndex.json")
     .then(response => {
       return response.json();
     })
@@ -17,6 +17,7 @@ whenLoaded = () => {
 
       let splitURL = location.href.split("?");
       console.log(splitURL);
+      
       if (splitURL[1].substring(0, 5) == "blog=") {
         let found = false;
         for (let i = 0; i < blogObjs.length; i++) {
@@ -30,12 +31,29 @@ whenLoaded = () => {
         }
 
         if (!found) {
-          $("#blogs").append("<p>The requested blog was not found</p>")
+          $("#blogs").append("<p>The requested blog was not found</p>");
+          $("#prev").css("display", "none");
+          $("#next").css("display", "none");
+        }else{
+	        $("#prev").attr("href", "?blog=" + ((splitURL[1].substring(5) > 0 && found) ? (splitURL[1].substring(5) - 1) : splitURL[1].substring(5)));
+	        $("#next").attr("href", "?blog=" + ((splitURL[1].substring(5) < blogObjs.length - 1 && found) ? (parseInt(splitURL[1].substring(5)) + 1) : splitURL[1].substring(5)));
         }
-
-        $("#prev").attr("href", "?blog=" + ((splitURL[1].substring(5) > 0 && found) ? (splitURL[1].substring(5) - 1) : splitURL[1].substring(5)));
-        $("#next").attr("href", "?blog=" + ((splitURL[1].substring(5) < blogObjs.length - 1 && found) ? (parseInt(splitURL[1].substring(5)) + 1) : splitURL[1].substring(5)));
-
+      }else if(splitURL[1].substring(0,10)=="blogStart="){
+    	  if(splitURL[2].substring(0,8) == "blogEnd="){
+	    	  let start = parseInt(splitURL[1].substring(10));
+	    	  let end = parseInt(splitURL[2].substring(8));
+	    	  
+	    	  for (let i = 0; i < blogObjs.length; i++) {
+	              if (blogObjs[i].id <= end && blogObjs[i].id >= start) {
+	                blogObjs[i].addBlogTo("#blogs", {
+	                  class: "blog",
+	                  style: "order:"+(i*1)
+	                });
+	              }
+	            }
+	    	  
+//	    	  alert(start+" "+end);
+	      }
       }
       console.log("Finished");
     })
