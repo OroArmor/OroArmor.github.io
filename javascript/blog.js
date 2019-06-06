b9405aa594343f2b9278229e00968d9187942c17
OroArmor <eliorona@live.com>
2019-06-05 21:05:24 -0700

    Update blog.js

--- javascript/blog.js
+++ javascript/blog.js
@@ -17,31 +17,34 @@ whenLoaded = () => {
 
       let splitURL = location.href.split("?");
       console.log(splitURL);
-      if(splitURL.length > 1){
-      if (splitURL[1].substring(0, 5) == "blog=") {
-        let found = false;
-        for (let i = 0; i < blogObjs.length; i++) {
-          if (blogObjs[i].id == splitURL[1].substring(5)) {
-            blogObjs[i].addBlogTo("#blogs", {
-              class: "blog",
-            });
-            found = true;
-            break;
+      if (splitURL.length > 1) {
+        if (splitURL[1].substring(0, 5) == "blog=") {
+          console.log("blog");
+          let found = false;
+          for (let i = 0; i < blogObjs.length; i++) {
+            if (blogObjs[i].id == splitURL[1].substring(5)) {
+              blogObjs[i].addBlogTo("#blogs", {
+                class: "blog",
+              });
+              found = true;
+              break;
+            }
           }
-        }
 
-        if (!found) {
-          $("#blogs").append("<p>The requested blog was not found</p>");
-          $("#prev").css("display", "none");
-          $("#next").css("display", "none");
-        } else {
-          $("#prev").attr("href", "?blog=" + ((splitURL[1].substring(5) > 0 && found) ? (splitURL[1].substring(5) - 1) : splitURL[1].substring(5)));
-          $("#next").attr("href", "?blog=" + ((splitURL[1].substring(5) < blogObjs.length - 1 && found) ? (parseInt(splitURL[1].substring(5)) + 1) : splitURL[1].substring(5)));
-        }
-      } else if (splitURL[1].substring(0, 10) == "blogStart=") {
-        if (splitURL[2].substring(0, 8) == "blogEnd=") {
+          if (!found) {
+            $("#blogs").append("<p>The requested blog was not found</p>");
+            $("#prev").css("display", "none");
+            $("#next").css("display", "none");
+          } else {
+            $("#prev").attr("href", "?blog=" + ((splitURL[1].substring(5) > 0 && found) ? (splitURL[1].substring(5) - 1) : splitURL[1].substring(5)));
+            $("#next").attr("href", "?blog=" + ((splitURL[1].substring(5) < blogObjs.length - 1 && found) ? (parseInt(splitURL[1].substring(5)) + 1) : splitURL[1].substring(5)));
+          }
+        } else if (splitURL[1].substring(0, 10) == "blogStart=") {
           let start = parseInt(splitURL[1].substring(10));
-          let end = parseInt(splitURL[2].substring(8));
+          let end = Math.max(start - 25, 0);
+          if (splitURL[2] && splitURL[2].substring(0, 8) == "blogEnd=") {
+            end = parseInt(splitURL[2].substring(8));
+          }
           $("#prev").css("display", "none");
           $("#next").css("display", "none");
           for (let i = 0; i < blogObjs.length; i++) {
@@ -52,25 +55,22 @@ whenLoaded = () => {
               });
             }
           }
-
-          //	    	  alert(start+" "+end);
         }
-      }
-      }else{ // default
-        let start = blogObjs.length-1;
-          let end = blogObjs.length-26;
-          for (let i = 0; i < blogObjs.length; i++) {
-            if (blogObjs[i].id >= end && blogObjs[i].id <= start) {
-              blogObjs[i].addBlogTo("#blogs", {
-                class: "blog",
-                style: "order:" + (i * 1)
-              });
-            }
+      } else { // default
+        let start = blogObjs.length - 1;
+        let end = blogObjs.length - 26;
+        for (let i = 0; i < blogObjs.length; i++) {
+          if (blogObjs[i].id >= end && blogObjs[i].id <= start) {
+            blogObjs[i].addBlogTo("#blogs", {
+              class: "blog",
+              style: "order:" + (i * 1)
+            });
           }
-        
-         $("#prev").attr("href", "?blog=" + ((end > 0) ? Math.max((end - 25),0) : end);
-          $("#next").attr("href", "?blog=" + ((start < blogObjs.length - 1) ? Math.min((start+25),blogObjs.length-1) : start));
-        
+        }
+
+        $("#prev").attr("href", "?blogStart=" + ((end < 0) ? Math.max((end - 25), 0) : end));
+        $("#next").attr("href", "?blogStart=" + ((start > blogObjs.length - 1) ? Math.min((start + 25), blogObjs.length - 1) : start));
+
       }
       console.log("Finished");
     })
